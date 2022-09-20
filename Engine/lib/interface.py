a5167d0a239c0500f5ef4db84433c9b9276120db
Sandro <34354689+Lainupcomputer@users.noreply.github.com>
2022-09-20 23:03:16 +0200

    added Parent animation class

--- Engine/lib/interface.py
+++ Engine/lib/interface.py
@@ -2,14 +2,32 @@
 import pygame
 
 
-class animation_no_collision:
+class version_information:
 
+    def __init__(self, screen, version):
+        self.screen = screen
+        self.font = pygame.font.SysFont("comicsansms", 12).render(f"Version:{version}", True, (55, 55, 55))
+        rect = self.font.get_rect()
+        rect.topleft = (20, 700)  # place
+        self.screen.blit(self.font, rect)
+
+
+class Animation:
     def __init__(self, screen, resource):
         self.screen = screen
         # get preloaded Animation images as list
-        self.animation = resource.load_animation
+        self.animation = resource
         # decides index of self.animation
         self.frame = 0
+
+    def tick(self):
+        self.frame += 1
+
+
+class animation_no_collision(Animation):
+
+    def __init__(self, screen, resource):
+        super().__init__(screen, resource)
 
     def draw(self, pos=(0, 0)):
         if self.frame < len(self.animation):
