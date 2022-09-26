a29b4f541dce2de0b7c5c1a4e31f004e5462409e
Sandro <34354689+Lainupcomputer@users.noreply.github.com>
2022-09-26 18:14:13 +0200

    init

--- Engine/lib/interface.py
+++ Engine/lib/interface.py
@@ -1,17 +1,76 @@
 #  Copyright (c) 2022.
 import pygame
 
+from Engine.lib.common import get_string_len
 
+
+# draw version Information
 class version_information:
 
     def __init__(self, screen, version):
+        position = (20, 700)
         self.screen = screen
-        self.font = pygame.font.SysFont("comicsansms", 12).render(f"Version:{version}", True, (55, 55, 55))
+        self.font = pygame.font.Font("Engine/assets/3Dventure.ttf", 16).render(f"version : {version}", True, (55, 55, 55))
         rect = self.font.get_rect()
-        rect.topleft = (20, 700)  # place
+        # place
+        rect.topleft = position
         self.screen.blit(self.font, rect)
 
 
+# draw text
+class Text:
+    def __init__(self, screen, position):
+        self.position = position
+        self.screen = screen
+        self.font = None
+
+    def render(self, text, size, color, font=None):
+        # if no font is set put default
+        if font is None:
+            self.font = pygame.font.Font("Engine/assets/3Dventure.ttf", size)
+        else:
+            self.font = pygame.font.Font(f"{font}", size)
+
+        text_obj = self.font.render(f"{text}", True, color)
+        rect = text_obj.get_rect()
+        rect.topleft = self.position
+        self.screen.blit(text_obj, rect)
+
+
+class Button:
+
+    def __init__(self, screen, resource, x, y, text, text_size=50, color=(55, 55, 55),
+                 text_center=True, font=None):
+        rect = resource.get_rect()
+        self.rect = pygame.Rect(rect[0] + x, rect[1] + y, rect[2], rect[3])
+        resource_size = resource.get_size()
+        print(resource_size)
+
+        # blit button_image
+        screen.blit(resource, (x, y))
+        # calculate position
+        offset = 10
+        if text_center:
+            # x
+            x - get_string_len(text) * text_size
+            x += offset
+
+            # y
+            half = resource_size[1] / 2 - text_size / 2
+            y += half
+        # draw text on top of background img
+        Text(screen, (x, y)).render(text, text_size, color, font)
+
+    def check_collision(self, mouse):
+        if self.rect.collidepoint(mouse):
+            return True
+        else:
+            return False
+
+
+
+
+# Animation Base Class
 class Animation:
     def __init__(self, screen, resource):
         self.screen = screen
@@ -47,3 +106,10 @@ class Arrow_Button(Animation):
 
         if self.frame >= len(self.animation):
             self.frame = -1
+
+
+class Toggle_Button:
+
+    def __init__(self, screen):
+        self.screen = screen
+
