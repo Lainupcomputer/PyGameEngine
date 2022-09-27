6eea10203b0418f5605af47e269b87a0d977a3a0
Sandro <34354689+Lainupcomputer@users.noreply.github.com>
2022-09-28 01:59:08 +0200

    added catch if resource not found

--- Engine/lib/storage.py
+++ Engine/lib/storage.py
@@ -27,25 +27,28 @@ class Resource:
     def __init__(self, skin=None):
         self.skin = skin
         if not skin:
+            try:
             # loading screen
-            self.load_background = pygame.image.load("Engine/assets/loading/default.png").convert()
-            self.load_animation = [pygame.image.load("Engine/assets/loading/1.png").convert_alpha(),
-                                   pygame.image.load("Engine/assets/loading/2.png").convert_alpha(),
-                                   pygame.image.load("Engine/assets/loading/3.png").convert_alpha()]
-            # main menu
-            self.menu_background = pygame.image.load("Engine/assets/main_menu/default.png").convert()
-            self.btn_background = pygame.image.load("Engine/assets/main_menu/btn_ui.png").convert_alpha()
-            # player
-            self.player_walk_images = [pygame.image.load("Engine/assets/player/player_walk_0.png").convert_alpha(),
-                                       pygame.image.load("Engine/assets/player/player_walk_1.png").convert_alpha(),
-                                       pygame.image.load("Engine/assets/player/player_walk_2.png").convert_alpha(),
-                                       pygame.image.load("Engine/assets/player/player_walk_3.png").convert_alpha()]
-            self.player_weapon = pygame.image.load("Engine/assets/player/player_weapon.png").convert_alpha()
+                self.load_background = pygame.image.load("Engine/assets/loading/default.png").convert()
+                self.load_animation = [pygame.image.load("Engine/assets/loading/1.png").convert_alpha(),
+                                       pygame.image.load("Engine/assets/loading/2.png").convert_alpha(),
+                                       pygame.image.load("Engine/assets/loading/3.png").convert_alpha()]
+                # main menu
+                self.menu_background = pygame.image.load("Engine/assets/main_menu/default.png").convert()
+                self.btn_background = pygame.image.load("Engine/assets/main_menu/btn_ui.png").convert_alpha()
+                # player
+                self.player_walk_images = [pygame.image.load("Engine/assets/player/player_walk_0.png").convert_alpha(),
+                                           pygame.image.load("Engine/assets/player/player_walk_1.png").convert_alpha(),
+                                           pygame.image.load("Engine/assets/player/player_walk_2.png").convert_alpha(),
+                                           pygame.image.load("Engine/assets/player/player_walk_3.png").convert_alpha()]
+                self.player_weapon = pygame.image.load("Engine/assets/player/player_weapon.png").convert_alpha()
 
 
 
-            # Ui
-            self.arrow_button_animation = [pygame.image.load("Engine/assets/ui/arrow_1.png").convert_alpha(),
-                                           pygame.image.load("Engine/assets/ui/arrow_2.png").convert_alpha(),
-                                           pygame.image.load("Engine/assets/ui/arrow_3.png").convert_alpha(),
-                                           pygame.image.load("Engine/assets/ui/arrow_4.png").convert_alpha()]
+                # Ui
+                self.arrow_button_animation = [pygame.image.load("Engine/assets/ui/arrow_1.png").convert_alpha(),
+                                               pygame.image.load("Engine/assets/ui/arrow_2.png").convert_alpha(),
+                                               pygame.image.load("Engine/assets/ui/arrow_3.png").convert_alpha(),
+                                               pygame.image.load("Engine/assets/ui/arrow_4.png").convert_alpha()]
+            except FileNotFoundError:
+                input("Verify Game Files!")
