26dc1f9f884069c491aded6ff94932cbd67cdc0d
Sandro <34354689+Lainupcomputer@users.noreply.github.com>
2022-10-05 07:26:55 +0200

    added version checker

--- start_engine.py
+++ start_engine.py
@@ -1,4 +1,29 @@
 from Engine import engine
+from Engine.lib.storage import Swap
+from urllib import request
 
-Game = engine.Engine()
-Game.main_loop()
+# create Swap
+swap = Swap()
+
+
+def get_external_version():
+    req = request.urlopen("https://github.com/Lainupcomputer/PyGameEngine/blob/master/version.txt")
+    data = req.read().decode('UTF-8')
+    s = data.find('_version==')
+    return data[s + 10:s + 17]
+
+
+def start_game(swap):
+    Game = engine.Engine(swap)
+    Game.main_loop()
+
+
+if swap.local_version == get_external_version():
+    # Game is Up to date // start game
+    start_game(swap)
+
+else:
+    print("Update Available\n"
+          "Update the Game to play.")
+
+
